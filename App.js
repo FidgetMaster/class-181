bc10bb265b0097c4f9c343daad3e14aac4d28293
FidgetMaster <75333039+FidgetMaster@users.noreply.github.com>
2022-06-13 12:53:57 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Main from "./screens/main"
+import React from "react"
+
+export default function App() {
+  return (
+    <Main/>
+  );
+}
